9f49b1f7c1cc0a63c4abac219c9412ad17d5058b
shamsherali-dev <shamsheralidev@gmail.com>
2026-05-07 09:47:41 +0500

    cd

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <div>
           <h1>Started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Edit Code<code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
